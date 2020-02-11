fbff9e84ef527f5895f7ceaa0c38bb85408da2c2
dadashi <khanim97@gmail.com>
2020-02-11 14:23:28 +0330

    1

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require("webpack");
+
+module.exports = {
+		mode: "production",
+		entry: "/Users/shervin/Desktop/webpack-test/src/app/index.js",
+		output: {
+			path:"/Users/shervin/Desktop/webpack-test/dist",
+			filename: "bundle.js",
+			publicPath: "dist",
+		},
+		devServer:{
+			inline: true,
+			contentBase:'./',
+			port: 3000,
+		},
+		
+		
+		
+module: {
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env','@babel/preset-react']
+        }
+      }
+    }
+  ]
+}
+
+
+
+}
